d15d2724c77618f40a6cc198e358232ee7b95559
EliasNada <51743419+EliasNada@users.noreply.github.com>
2019-12-29 00:18:37 +0200

    Solution

--- cli.php
+++ cli.php
@@ -10,6 +10,6 @@ $first = fgets($input);
 echo "Second Text: \n";
 $input = fopen("php://stdin", "r");
 $second = fgets($input);
-echo "Levenshtein: ".Helper::instantiate('Levenshtein', $first, $second)." operations made \n";
+echo "Levenshtein: ".Helper::instantiate('Levenshtein', $first, $second)." operations needed \n";
 
 ?>
